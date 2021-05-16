ff5ecdbe4418331a93291e17e3404c4259997c18
SSgumS <sssafais@hotmail.com>
2021-05-16 23:22:07 +0430

    email dkim support

--- FProject/Server/Services/Email/EmailService.cs
+++ FProject/Server/Services/Email/EmailService.cs
@@ -2,6 +2,8 @@
 using MailKit.Net.Smtp;
 using Microsoft.Extensions.Options;
 using MimeKit;
+using MimeKit.Cryptography;
+using Org.BouncyCastle.Crypto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +14,17 @@ namespace FProject.Server.Services
     public class EmailService
     {
         private MailServerOptions _options;
+        private DkimSigner _signer;
+        private HeaderId[] _headers;
 
         public EmailService(IOptions<MailServerOptions> options)
         {
             _options = options.Value;
+            _headers = new HeaderId[] { HeaderId.From, HeaderId.Subject, HeaderId.Date };
+            if (_options.PKPath is not null)
+            {
+                _signer = new DkimSigner(_options.PKPath, _options.Domain, _options.Selector);
+            }
         }
 
         public async Task SendEmailAsync(EmailMessage message)
@@ -31,6 +40,11 @@ namespace FProject.Server.Services
             using var smtp = new SmtpClient();
             smtp.Connect(_options.SmtpServer, _options.Port);
             smtp.Authenticate(_options.Username, _options.Password);
+            if (_signer is not null)
+            {
+                email.Prepare(EncodingConstraint.EightBit);
+                _signer.Sign(email, _headers);
+            }
             await smtp.SendAsync(email);
             smtp.Disconnect(true);
         }
@@ -44,5 +58,8 @@ namespace FProject.Server.Services
         public string DisplayName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string PKPath { get; set; }
+        public string Domain { get; set; }
+        public string Selector { get; set; }
     }
 }

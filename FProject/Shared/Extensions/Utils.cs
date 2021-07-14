baea2ae2143abb7ec6b51c9036c460771830983f
SSgumS <sssafais@hotmail.com>
2021-07-14 17:08:15 +0430

    persian number

--- FProject/Shared/Extensions/Utils.cs
+++ FProject/Shared/Extensions/Utils.cs
@@ -123,5 +123,17 @@ namespace FProject.Shared.Extensions
                 _ => throw new NotSupportedException()
             };
         }
+
+        private static string[] EngDigits = new[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private static string[] FaDigits = new[] { "۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹" };
+        public static string ToPersianNumber(this object number)
+        {
+            var result = number.ToString();
+            for (int i = 0; i <= 9; i++)
+            {
+                result = result.Replace(EngDigits[i], FaDigits[i]);
+            }
+            return result;
+        }
     }
 }
